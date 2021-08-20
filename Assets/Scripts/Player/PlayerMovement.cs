b6beb9fe88108904c431ef0fe737926b9d40ed6f
sabiri253 <sabirim52@gmail.com>
2021-08-20 15:16:05 +0100

    PlayerMovement

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -1,12 +1,16 @@
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D),typeof(Collider2D))]
 public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] float speed = 7;
+
+    Rigidbody2D rb2d;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        rb2d = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
