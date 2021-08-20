a53bfdd60da526acd8db80a173265042a735b8d0
sabiri253 <sabirim52@gmail.com>
2021-08-20 15:23:36 +0100

    EnemyBaseClass

---
+++ Assets/Scripts/Enemies/EnemieBaseClass.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public abstract class EnemieBaseClass : MonoBehaviour
+{
+    protected abstract void Follow();
+    protected abstract void Attack();
+
+   // WE ARE STILL NOT SURE IF THE ENEMY IS GOING TO BE AN AGRESSIVE ENTITY BUT LET'S STAY SAFE AND ADD  THIS CLASS
+
+}
