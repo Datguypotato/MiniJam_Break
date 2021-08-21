9b343dea6afa9353b4f146671e206020cb5f3902
Gordon home PC <aardappel156@gmail.com>
2021-08-21 03:43:06 +0200

    Finished pouring liquid
    Going to probably rework some stuff next day

--- Assets/Scripts/Fluid/CupBehaviour.cs
+++ Assets/Scripts/Fluid/CupBehaviour.cs
@@ -2,11 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// This is the player cup for pouring stuff in other cups
+/// </summary>
 public class CupBehaviour : MonoBehaviour
 {
     private ParticleGenerator particleGenerator;
+    [SerializeField] private float rotateSpeed = 1;
+    [SerializeField] private float tiltLeftMax = 110;
+    [SerializeField] private float titltRightMax = -10;
+    [SerializeField] private float pourSpot = 100;
+
     [SerializeField] private Vector3 cupOffset;
     [SerializeField] private Camera cam;
+    private bool isPooring;
 
     private void Start()
     {
@@ -16,13 +25,40 @@ public class CupBehaviour : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
+        // follow mousecursor
         transform.position = cam.ScreenToWorldPoint(Input.mousePosition) - cupOffset;
 
-        if (Input.GetKey(KeyCode.A))
-            transform.Rotate(new Vector3(0, 0, 1f));
-        if (Input.GetKey(KeyCode.D))
-            transform.Rotate(new Vector3(0, 0, -1f));
+        // tilting the cup
+        if (Input.GetMouseButtonDown(0))
+        {
+            StartCoroutine(Pour());
+            Debug.Log("click");
+        }
 
-        particleGenerator.isActivated = (transform.rotation.eulerAngles.z > 100);
+        //Debug.Log(transform.rotation.eulerAngles.z + " > " + titltRightMax);
+        // pour only if it is rotate so far
+        particleGenerator.isActivated = isPooring;
+    }
+
+    private IEnumerator Pour()
+    {
+        if (isPooring)
+        {
+            while (transform.rotation.eulerAngles.z > 1)
+            {
+                transform.Rotate(new Vector3(0, 0, -1));
+                yield return new WaitForEndOfFrame();
+            }
+            isPooring = false;
+        }
+        else
+        {
+            while (transform.rotation.eulerAngles.z < 100)
+            {
+                transform.Rotate(new Vector3(0, 0, 1));
+                yield return new WaitForEndOfFrame();
+            }
+            isPooring = true;
+        }
     }
 }
